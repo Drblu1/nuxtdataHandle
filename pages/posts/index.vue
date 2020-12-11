2a2019df95bb3ac7e4c628de2f144624d3d2bfc0
yyavo <yann.yavo@i-run.fr>
2020-12-11 12:30:59 +0100

    Asyn data in blog

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts"/>
   </div>
 </template>
 
@@ -10,6 +10,28 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
+  },
+  asyncData(context, callback) {
+    setTimeout( () => {
+      //THIS !!!!!!
+      callback(null, {
+        loadedPosts: [
+          {
+            id: '1',
+            title:'wejdene',
+            previewText: 'this is our first post',
+            thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+          },
+          {
+            id: '2',
+            title:'Best app ever',
+            previewText: 'This really changed my life <3',
+            thumbnail: 'https://lh3.googleusercontent.com/5PBEtJH72OUM9pbwnZC-MfCcb5-XGVTWLbNjNLbrLmbNmXe-p7geFws6vvdXqddAGvM'
+          }
+        ]
+      })
+
+    },1500)
   }
 }
 </script>
